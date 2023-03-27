b3c2e767ca7f26876236e0d2e67b83c43b06f7cb
anjaliruth <ruthanjali0211@gmail.com>
2023-03-27 11:49:12 +0800

    Fetched data

--- main.js
+++ main.js
@@ -1,65 +1,15 @@
-//gonna fetch data from each API 
-//async function
-//fetch and wait from URL 
-//Console log response
-// console.log("linked")
-// async function pokemon() {
-  //     const response = await fetch ("https://pokeapi.co/api/v2/pokemon/1/");
-  //     console.log(response)
-  //     const data = await response.json()
-  //     console.log(data)
+//Dictionary API
+//Step 1: View data
+//create async function await fetch
+//await response
+// console log Response
+//call function
+
+async function getData() {
+  let response = await fetch (`https://api.dictionaryapi.dev/api/v2/entries/en_GB/hello`)
+  let data = await response.json()
+  console.log(data)
+  console.log(data[0].meanings[0].definitions[0].definition)
+}
+getData()
   
-  // }
-  
-  function addToFavourites(str) {
-     let jokeList = document.createElement("li")
-     jokeList.textContent = str
-     document.querySelector("#favourites").appendChild(jokeList)
-   }
-
-   
-   let theJoke = "";
-   async function dadJokes() {
-     const response = await fetch("https://icanhazdadjoke.com/", {
-       headers: { accept: "application/json" },
-      });
-      console.log(response)
-      const gettingJoke = await response.json()
-      console.log(gettingJoke)
-      
-      theJoke = document.querySelector("#dadJoke");
-      theJoke.textContent = gettingJoke.joke;
-      
-    }
-    document.querySelector('#click-me').addEventListener('click', dadJokes);
-    
-    //if input = yes, add to favourites
-    //put this function inside the async function with (theJoke.textContent)
-    // add event listener to YES button
-    let favouriteButton = document.querySelector("#yes-favourites")
-    favouriteButton.addEventListener("click", () => addToFavourites(theJoke.textContent))
-    //refresh
-const refreshButton = document.querySelector("#refresh");
-refreshButton.addEventListener("click", () => {
-  const jokeList = document.querySelector("#favourites");
-  
-  while (jokeList.firstChild) {
-    jokeList.removeChild(jokeList.firstChild);
-  }
-});
-
-//  function refresh (event) {
-//   document.querySelector("#favourites").textContent = "";
-//  }
- //document.querySelector("refresh").addEventListener("click", refresh);
- 
- // create submit button
- //if rated highest, ask if you want to save to fav 
- //to create a favourites list
-
-  //create fav list, with fav dad jokes
-
-    
-
-
-
